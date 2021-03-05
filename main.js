36dc7510af4a8b39423fde1c21669fba106d7c55
aboayube <snuora2019@gmail.com>
2021-03-06 00:53:08 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const navMenu=document.getElementById('nav-menu'),
+      toggleMenu=document.getElementById('nav-toggle'),
+      closeMenu=document.getElementById('nav-close');
+
+const navLink=document.querySelectorAll('.nav__link');
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show');
+})
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove('show');
+})
+function linkActive(){
+    navMenu.classList.remove('show');
+}
+navLink.forEach(n=>n.addEventListener('click',linkActive));
+
+const sections=document.querySelectorAll('section[id]');
+window.addEventListener('scroll',scrollActive);
+function scrollActive(){
+    const scrollY=window.pageYOffset
+    sections.forEach(current=>{
+        const sectionHeight=current.offsetHeight
+        const sectionTop=current.offsetTop - 50
+        sectionId=current.getAttribute('id')
+        
+        if(scrollY > sectionTop & scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add('active');
+        }else{
+            
+            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.remove('active');    
+        }
+    })
+}
